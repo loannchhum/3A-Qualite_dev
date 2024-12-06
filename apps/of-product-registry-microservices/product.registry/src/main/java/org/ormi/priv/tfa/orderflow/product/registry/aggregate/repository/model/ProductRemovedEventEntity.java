9cac0dde2b6b2921fe69fecc070d42af8b29b183
Chhum--Moxel Loann <o22204226@or-iut-i201-f27.iut45.univ-orleans.fr>
2024-12-06 16:36:12 +0100

    correction runtime exceptions + payload

--- apps/of-product-registry-microservices/product.registry/src/main/java/org/ormi/priv/tfa/orderflow/product/registry/aggregate/repository/model/ProductRemovedEventEntity.java
+++ apps/of-product-registry-microservices/product.registry/src/main/java/org/ormi/priv/tfa/orderflow/product/registry/aggregate/repository/model/ProductRemovedEventEntity.java
@@ -1,17 +1,15 @@
 package org.ormi.priv.tfa.orderflow.product.registry.aggregate.repository.model;
 
+/**
+ * Entity representing a product removal event in the product registry.
+ */
 public class ProductRemovedEventEntity extends ProductRegistryEventEntity {
   static final String EVENT_TYPE = "ProductRemoved";
 
   /**
-   * Payload for the event.
+   * Payload for the event, implemented as a record.
    */
-  public static class Payload {
-    /**
-     * The id of the product.
-     */
-    public String productId;
-  }
+  public record Payload(String productId) {}
 
   /**
    * The payload for the event.
@@ -22,5 +20,4 @@ public class ProductRemovedEventEntity extends ProductRegistryEventEntity {
   public String getEventType() {
     return EVENT_TYPE;
   }
-  
 }
