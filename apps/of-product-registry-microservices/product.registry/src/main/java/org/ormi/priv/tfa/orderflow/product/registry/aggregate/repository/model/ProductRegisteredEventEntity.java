ea6beb7c03af96118ee67621660011bca204cb74
Chhum--Moxel Loann <o22204226@or-iut-i201-f27.iut45.univ-orleans.fr>
2024-12-13 16:00:54 +0100

    modification des champs en privés

--- apps/of-product-registry-microservices/product.registry/src/main/java/org/ormi/priv/tfa/orderflow/product/registry/aggregate/repository/model/ProductRegisteredEventEntity.java
+++ apps/of-product-registry-microservices/product.registry/src/main/java/org/ormi/priv/tfa/orderflow/product/registry/aggregate/repository/model/ProductRegisteredEventEntity.java
@@ -1,33 +1,106 @@
 package org.ormi.priv.tfa.orderflow.product.registry.aggregate.repository.model;
 
 public class ProductRegisteredEventEntity extends ProductRegistryEventEntity {
-  static final String EVENT_TYPE = "ProductRegistered";
 
-  /**
-   * Payload for the event.
-   */
-  public static class Payload {
-    /**
-     * The id of the product.
-     */
-    public String productId;
-    /**
-     * The name of the product.
-     */
-    public String name;
-    /**
-     * The description of the product.
-     */
-    public String productDescription;
-  }
+    static final String EVENT_TYPE = "ProductRegistered";
 
-  /**
-   * The payload for the event.
-   */
-  public Payload payload;
+    /**
+     * Payload for the event.
+     */
+    public static class Payload {
+        /**
+         * The id of the product.
+         */
+        private String productId;
+        /**
+         * The name of the product.
+         */
+        private String name;
+        /**
+         * The description of the product.
+         */
+        private String productDescription;
 
-  @Override
-  public String getEventType() {
-    return EVENT_TYPE;
-  }
+        /**
+         * Get the product ID.
+         * 
+         * @return the productId
+         */
+        public String getProductId() {
+            return productId;
+        }
+
+        /**
+         * Set the product ID.
+         * 
+         * @param productId the productId to set
+         */
+        public void setProductId(String productId) {
+            this.productId = productId;
+        }
+
+        /**
+         * Get the name of the product.
+         * 
+         * @return the name
+         */
+        public String getName() {
+            return name;
+        }
+
+        /**
+         * Set the name of the product.
+         * 
+         * @param name the name to set
+         */
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        /**
+         * Get the product description.
+         * 
+         * @return the productDescription
+         */
+        public String getProductDescription() {
+            return productDescription;
+        }
+
+        /**
+         * Set the product description.
+         * 
+         * @param productDescription the productDescription to set
+         */
+        public void setProductDescription(String productDescription) {
+            this.productDescription = productDescription;
+        }
+    }
+
+    /**
+     * The payload for the event.
+     */
+    private Payload payload;
+
+    /**
+     * Get the payload for the event.
+     * 
+     * @return the payload
+     */
+    public Payload getPayload() {
+        return payload;
+    }
+
+    /**
+     * Set the payload for the event.
+     * 
+     * @param payload the payload to set
+     */
+    public void setPayload(Payload payload) {
+        this.payload = payload;
+    }
+
+    @Override
+    public String getEventType() {
+        return EVENT_TYPE;
+    }
 }
--- apps/of-product-registry-microservices/product.registry/src/main/java/org/ormi/priv/tfa/orderflow/product/registry/aggregate/repository/model/ProductRemovedEventEntity.java
+++ apps/of-product-registry-microservices/product.registry/src/main/java/org/ormi/priv/tfa/orderflow/product/registry/aggregate/repository/model/ProductRemovedEventEntity.java
@@ -1,26 +1,62 @@
 package org.ormi.priv.tfa.orderflow.product.registry.aggregate.repository.model;
 
 public class ProductRemovedEventEntity extends ProductRegistryEventEntity {
-  static final String EVENT_TYPE = "ProductRemoved";
 
-  /**
-   * Payload for the event.
-   */
-  public static class Payload {
+    static final String EVENT_TYPE = "ProductRemoved";
+
     /**
-     * The id of the product.
+     * Payload for the event.
      */
-    public String productId;
-  }
+    public static class Payload {
+        /**
+         * The id of the product.
+         */
+        private String productId;
 
-  /**
-   * The payload for the event.
-   */
-  public Payload payload;
+        /**
+         * Get the product id.
+         * 
+         * @return the product id
+         */
+        public String getProductId() {
+            return productId;
+        }
 
-  @Override
-  public String getEventType() {
-    return EVENT_TYPE;
-  }
-  
+        /**
+         * Set the product id.
+         * 
+         * @param productId the product id to set
+         */
+        public void setProductId(String productId) {
+            this.productId = productId;
+        }
+    }
+
+    /**
+     * The payload for the event.
+     */
+    private Payload payload;
+
+    /**
+     * Get the payload for the event.
+     * 
+     * @return the payload
+     */
+    public Payload getPayload() {
+        return payload;
+    }
+
+    /**
+     * Set the payload for the event.
+     * 
+     * @param payload the payload to set
+     */
+    public void setPayload(Payload payload) {
+        this.payload = payload;
+    }
+
+    @Override
+    public String getEventType() {
+        return EVENT_TYPE;
+    }
 }
